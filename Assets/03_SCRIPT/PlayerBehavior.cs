68478a9368da53d1f36e0777a87b50f7c4671435
zessirb <zessirb@gmail.com>
2017-12-02 12:45:30 +0100

    Ajout script de déplacement personnage et jouet

---
+++ Assets/03_SCRIPT/PlayerBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        move();
+	}
+    
+    static float speed = 8.00f;
+
+    void move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        if (moveHorizontal != 0 || moveVertical != 0)
+        {
+            if (moveHorizontal != 0 && moveVertical != 0)
+            {
+                moveHorizontal *= Mathf.Sqrt(2) / 2;
+                moveVertical *= Mathf.Sqrt(2) / 2;
+            }
+            float deltaTime = Time.deltaTime;
+
+            Vector2 moveVector = new Vector2(moveHorizontal * deltaTime * speed, moveVertical * deltaTime * speed);
+            gameObject.transform.Translate(moveVector, Space.World);
+        }
+    }
+}
---
+++ Assets/03_SCRIPT/ToyBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ToyBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        slide();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        collidingObjects++;
+    }
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        collidingObjects--;
+    }
+
+    static float speed = 1.00f;
+    int collidingObjects = 0;
+    
+    void slide()
+    {
+        if (collidingObjects == 0)
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speed);
+        }
+    }
+}
