b7bc7df4521825ad373c1fa2eeebb83ace6a5cae
Raiponz <spooki.leserpent@orange.fr>
2017-12-02 23:47:51 +0100

    systeme d'infobulle complet

--- Assets/Resources/03_SCRIPT/GameBehavior.cs
+++ Assets/Resources/03_SCRIPT/GameBehavior.cs
@@ -9,9 +9,9 @@ public class GameBehavior : MonoBehaviour {
 	void Start () {
         ToyList = new List<Toy>
         {
-            new Toy("Croque monsieur","this sandwish is already bitted","small","toy_croq"),
-            new Toy("Wooden horse","fun toy !","big","toy_horse"),
-            new Toy("mommy's duck","how did this arrived here","","toy_momysduck")
+            new Toy("Croque monsieur","this sandwish is already bitten",1,"toy_croq"),
+            new Toy("Wooden horse","fun toy !",3,"toy_horse"),
+            new Toy("mommy's duck","how did this arrived here",2,"toy_momysduck")
         };
         InvokeRepeating("CreateToy", 2.0f, 3.0f);
     }
@@ -29,9 +29,7 @@ public class GameBehavior : MonoBehaviour {
 
         GameObject toyObject = GameObject.Instantiate(Resources.Load("10_PREFABS/croque"), this.transform.position, this.transform.rotation) as GameObject;
         Toy toy = ToyList[index];
-        toyObject.GetComponent<ToyBehaviour>().name = toy.name;
-        toyObject.GetComponent<ToyBehaviour>().size = toy.size;
-        toyObject.GetComponent<ToyBehaviour>().description = toy.description;
+        toyObject.GetComponent<ToyBehaviour>().toy = toy;
         toyObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("09_TEXTURE/"+ toy.spriteName);
 
     }
--- Assets/Resources/03_SCRIPT/Toy.cs
+++ Assets/Resources/03_SCRIPT/Toy.cs
@@ -1,19 +1,33 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
 
-public class Toy 
+public class Toy
 {
+    public Dictionary<int, string> sizeLabel = new Dictionary<int, string>()
+    {
+        {1,"Small" },
+        {2,"Medium" },
+        {3,"Large" }
+    };
+    
     public string name;
     public string description;
-    public string size;
+    public int size;
     public string spriteName;
 
-    public Toy(string p_name,string p_description, string p_size, string p_spriteName)
+    public Toy() { }
+    public Toy(string p_name,string p_description, int p_size, string p_spriteName)
     {
         this.name = p_name;
         this.description = p_description;
         this.size = p_size;
         this.spriteName = p_spriteName;
+    }
+
+   public string GetLabelSize()
+    {
+        return sizeLabel[this.size];
     }
 
 
