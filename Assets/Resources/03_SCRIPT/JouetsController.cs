abefdee94e64c13e0ea7ab4381bb6eb8da6a05ed
Raiponz <spooki.leserpent@orange.fr>
2017-12-02 17:46:18 +0100

    refactoring de masse

---
+++ Assets/Resources/03_SCRIPT/JouetsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JouetsController : MonoBehaviour {
+
+
+    SpriteRenderer sp;
+     void Start()
+    {
+        Debug.Log("start");
+
+       // GameObject gameObject = Instantiate
+        sp = this.gameObject.GetComponent<SpriteRenderer>();
+        Sprite o = Resources.Load<Sprite>("09_TEXTURE/croque");
+
+        sp.sprite = o;
+        if (o == null) Debug.Log("Load failed");
+    }
+    private void Update()
+    {
+      //  sp.sprite = Sprite.Create("croque.png");
+    }
+
+
+}
